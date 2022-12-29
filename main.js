b20ef3cca751f0d4f56812c67799622d387b206c
이정수 <jslee3@suprema.co.kr>
2022-12-29 14:55:00 +0900

    디비서버 분리

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const conn = require("./core/database");
+const conn = require("./core/database");
 
 const app = express()
     .use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }))
@@ -10,12 +10,18 @@ const app = express()
 app.get("/", function (request, response) {
   response.render("index.html");
 });
-
+app.get("/test", function (request, response) {
+  response.sendStatus(123);
+});
 app.post("/create", function (request, response) {});
 
-app.put("/modify", function (request, response) {});
+app.put("/modify", function (request, response) {
+  console.log(request)
+  response.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
+});
 
 app.delete("/delete", function (request, response) {});
+
 app.listen(3000, function () {
   console.log("server is running on port 3000");
 });
