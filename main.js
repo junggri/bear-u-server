7e2c49ef5c8357725dfaa3c91cb2f4248c6da62e
junggri <gkgkgk6933@gmail.com>
2023-01-09 21:56:52 +0900

    add

--- main.js
+++ main.js
@@ -12,9 +12,15 @@ app.get("/", function (request, response) {
 });
 
 app.post("/create", function (request, response) {
-  console.log(request.body);
-  response.status(200).json(123)
-  conn.query("INSERT INTO LIST (title, description, filename, createA)")
+  conn.query("INSERT INTO LIST (title, description, filename, createdAt,updatedAt) values (?,?,?,now(),now());",[request.body.title,request.body.description,request.body.content],function(err,result){
+    if(err){
+      console.error(err)
+    }
+    console.log(result.affectedRows)
+    if(result.affectedRows === 1){
+      response.status(200).json({status: 'success'})
+    }
+  })
 });
 
 app.put("/modify", function (request, response) {
@@ -27,6 +33,11 @@ app.delete("/delete", function (request, response) {});
 
 app.get("/lists",  function(request,response){
   conn.query("SELECT * FROM list",function(err,result){
+    if(err){
+      console.log(err)
+      response.status(500).json({message: "request fail"});
+    }
+    console.log(result)
     response.status(200).json(result);
   })
 })
